ddc8f4c245a7d9e50fab41f4de447d638aac5d5c
Matthews24 <99995715+Mateus2022@users.noreply.github.com>
2024-10-18 19:35:49 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -23,10 +23,7 @@ make -j20 O=out \
                       STRIP=llvm-strip \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-                      CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
-                      CONFIG_VIRTUALIZATION=y \
-                      CONFIG_KVM=y
-
+                      CONFIG_SECTION_MISMATCH_WARN_ONLY=y 
 }
 function zupload()
 {
